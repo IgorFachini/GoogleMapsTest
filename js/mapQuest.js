17d7957589f659d4f244b35d8a1e6682301a39aa
igorfachini <igor.fachini@catolicasc.org.br>
2018-01-17 15:47:25 -0200

    direction example and marker follow current position

--- js/mapQuest.js
+++ js/mapQuest.js
@@ -1,43 +1,62 @@
 var mapsContainerData = new Vue({
-  el: '#mapsContainer',
+  el: "#mapsContainer",
   data: {
     accuracy: 0,
     speed: 0,
     heading: 0
   }
 });
+var L;
+L.mapquest.key = "ItgV4yIhtPxpn3s4hBdHqIU5dupXO9WK";
 
-
-L.mapquest.key = 'ItgV4yIhtPxpn3s4hBdHqIU5dupXO9WK';
-
-var mapData = L.mapquest.map('map', {
+var mapData = L.mapquest.map("map", {
   center: [-26.4665992733309, -49.11446034908295],
-  layers: L.mapquest.tileLayer('map'),
+  layers: L.mapquest.tileLayer("map"),
   zoom: 12
 });
 
 mapData.addControl(L.mapquest.control());
 
+var currentLocation = L.marker([-26.4665992733309, -49.11446034908295], {
+  icon: L.mapquest.icons.marker(),
+  draggable: false
+})
+  .bindPopup("Denver, CO")
+  .addTo(mapData);
+
 // watch postion
-var id, target, options;
+var id, target, options, notFoundInitLocation = true;
 
 function success(pos) {
-  console.log("watch",pos)
+  console.log("watch", pos);
   var crd = pos.coords;
   mapsContainerData.accuracy = crd.accuracy;
   mapsContainerData.heading = crd.heading;
   mapsContainerData.speed = crd.speed;
-
   // navigator.geolocation.clearWatch(id);
-
+  currentLocation.setLatLng([crd.latitude, crd.longitude]);
+  mapData.setView([crd.latitude, crd.longitude]);
+  mapData.setZoom(17);
+  if(notFoundInitLocation){
+    initDirection(crd);
+  }
+  notFoundInitLocation = false;
 }
 
+function initDirection(crd) {
+  L.mapquest.directions().route({
+    start: [crd.latitude, crd.longitude],
+    end: [-26.276394099999997,-48.8749733]
+  });
+}
+
+
 function error(err) {
-  console.warn(`ERRO(' ${ err.code } '): ` + err.message);
+  console.warn(`ERRO(' ${err.code} '): ` + err.message);
 }
 
 target = {
-  latitude : 0,
+  latitude: 0,
   longitude: 0
 };
 
